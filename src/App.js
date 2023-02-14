1289effadd7ec59fa39af0d6c94125e13a999443
Rushali <rushali.chandora@msbcgroup.com>
2023-02-14 12:51:40 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Portfolio from './Components/React/Portfolio/Portfolio';
+import ScriptData from './Components/Potfolio/script'
+
+
+function App() {
+  return (
+    <div className="App">
+<Portfolio/>
+<ScriptData/>
+    </div>
+  );
+}
+
+export default App;
